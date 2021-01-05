c0ebb4b066f0069181371a60c66d4c53c99070f9
yaohui <330833493@qq.com>
2021-01-05 15:48:19 +0800

    第一次数据提交

---
+++ module01/src/com/yh/Test01.java
@@ -0,0 +1,22 @@
+package com.yh;
+
+import java.io.Serializable;
+
+public class Test01 implements Serializable {
+
+    private static final long serialVersionUID = 5010959790173085679L;
+    private String name;
+    private String age;
+
+    public static void main(String[] args) {
+        String a = "hi yaohui";
+        System.out.println(a);
+        for (int i = 0; i < 100; i++) {
+            String b = "";
+        }
+
+        if (a != null) {
+
+        }
+    }
+}
---
+++ src/test.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Date;
+
+public class test {
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("hello word");
+        new Date();
+        new ArrayList();
+        new String();
+    }
+    public void eat(){
+
+    }
+}
