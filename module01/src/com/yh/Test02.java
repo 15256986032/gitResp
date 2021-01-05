729215ee1c96499e25ea0e63f87aded10d6f4557
yaohui <330833493@qq.com>
2021-01-05 20:28:35 +0800

    上传

---
+++ module01/src/com/yh/Test02.java
@@ -0,0 +1,14 @@
+package com.yh;
+
+public class Test02 extends Test01{
+
+    public static void main(String[] args) {
+        String a = "hi yaohui";
+        System.out.println(a);
+    }
+
+    public static void getName() {
+        System.out.println("hello");
+
+    }
+}
